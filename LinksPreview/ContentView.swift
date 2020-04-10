85d6f9132a6b9caa17699833ca408fb384e64b8d
priyans <priyans05@gmail.com>
2020-04-11 06:18:28 +1000

    Initial Commit

---
+++ LinksPreview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LinksPreview
+//
+//  Created by PRIYANS on 11/4/20.
+//  Copyright © 2020 PRIYANS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
